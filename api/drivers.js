7531f9af15eef1b0b3ef1af85bd5798e68eb1317
loadedbowls <jashanpreetsingh192@gmail.com>
2026-07-24 12:17:09 +0200

    Update drivers.js

--- api/drivers.js
+++ api/drivers.js
@@ -20,6 +20,9 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-admin-secret");
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
 
   if (req.method === "OPTIONS") return res.status(204).end();
 
